5273a965d5353de32e57d17ea4df3761be6f1e33
LeeJaeyoung <aao95@naver.com>
2022-04-25 10:09:07 +0900

    [BE] :sparkles: feat: create spring boot

---
+++ Backend/src/main/java/com/ssafy/ssam/ssam_backend/SsamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssam.ssam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsamBackendApplication.class, args);
+    }
+
+}
