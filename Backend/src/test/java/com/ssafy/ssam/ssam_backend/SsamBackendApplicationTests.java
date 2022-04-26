2b5bca0c0a54eaa802365f3661187c0ba1e78582
tekies0909@gmail.com <tekies0909@gmail.com>
2022-04-26 15:59:15 +0900

    [BE] ✨ feat: springboot test 주석처리

--- Backend/src/test/java/com/ssafy/ssam/ssam_backend/SsamBackendApplicationTests.java
+++ Backend/src/test/java/com/ssafy/ssam/ssam_backend/SsamBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.ssam.ssam_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SsamBackendApplicationTests {
 
     @Test
