7576dcd380eddf8bac6fa9f01788279ee0fd7fc0
조성한 <toor21@naver.com>
2022-04-26 06:32:26 +0000

    [S06P31A403-100] Merge: develop ← feature/fe/header

---
+++ Frontend/src/Theme.js
@@ -0,0 +1,16 @@
+// 테마를 여기서 정의하시면 됩니다.
+
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#37467B',
+    },
+    secondary: {
+      main: '#638493'
+    },
+  }
+})
+
+export default theme
