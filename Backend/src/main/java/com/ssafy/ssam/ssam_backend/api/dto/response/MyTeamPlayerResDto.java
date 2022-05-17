b8320f420407cb0872d4e4a6c046d4da8e64c1b3
LeeJaeyoung <aao95@naver.com>
2022-05-17 18:06:40 +0900

    [BE] :sparkles: feat: 나만의 팀 엔티티 수정 및 dto, service 수정
    - 엔티티 관계 설정 변경 : 연도별 기록과 나만의 팀 선수목록 연관관계 설정
    - dto, service 변경 : 위 엔티티 변경에 맞추어 reqDto, resDto,
      Service함수들 수정

--- Backend/src/main/java/com/ssafy/ssam/ssam_backend/api/dto/response/MyTeamPlayerResDto.java
+++ Backend/src/main/java/com/ssafy/ssam/ssam_backend/api/dto/response/MyTeamPlayerResDto.java
@@ -10,18 +10,34 @@ import lombok.NoArgsConstructor;
 public class MyTeamPlayerResDto {
     @ApiModelProperty(value = "DB에 등록된 myteamplayer id")
     private Long myTeamPlayerId;
-    @ApiModelProperty(value = "DB에 등록된 선수 id")
-    private Long playerId;
+    @ApiModelProperty(value = "연도별 선수 기록의 id", notes = "투수인지 타자인지는 밑에서 판단")
+    private Long statusId;
+    @ApiModelProperty(value = "선수이름")
+    private String name;
     @ApiModelProperty(value = "선수 등번호")
     private int backNumber;
     @ApiModelProperty(value = "타순")
     private String battingOrder;
     @ApiModelProperty(value = "수비 포지션")
     private String defensePosition;
+    @ApiModelProperty(value="투수 or 타자")
+    private String pitcherOrHitter;
+    @ApiModelProperty(value = "연도")
+    private String years;
 
     public MyTeamPlayerResDto (MyTeamPlayer myTeamPlayer){
         this.myTeamPlayerId=myTeamPlayer.getMyTeamPlayerId();
-        this.playerId = myTeamPlayer.getPlayer().getPlayerId();
+        this.pitcherOrHitter = myTeamPlayer.getRole();
+        if(pitcherOrHitter.equals("Hitter")){
+            this.statusId = myTeamPlayer.getHitterYearsStatus().getHitterYearsSId();
+            this.years = myTeamPlayer.getHitterYearsStatus().getYears();
+            this.name = myTeamPlayer.getHitterYearsStatus().getPlayer().getPlayerName();
+        }
+        else{
+            this.statusId = myTeamPlayer.getPitcherYearsStatus().getPitcherYearsSId();
+            this.years = myTeamPlayer.getPitcherYearsStatus().getYears();
+            this.name = myTeamPlayer.getPitcherYearsStatus().getPlayer().getPlayerName();
+        }
         this.backNumber = myTeamPlayer.getBackNumber();
         this.battingOrder = myTeamPlayer.getBattingOrder();
         this.defensePosition = myTeamPlayer.getDefensePosition();
