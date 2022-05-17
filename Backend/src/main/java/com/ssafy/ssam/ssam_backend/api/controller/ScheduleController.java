7324074fe6976c43cfc1f472089f2d1ebbc5df35
여율 <dbfm2222@gmail.com>
2022-05-17 17:30:33 +0900

    :hammer: refactor: CORS 어노테이션 추가

--- Backend/src/main/java/com/ssafy/ssam/ssam_backend/api/controller/ScheduleController.java
+++ Backend/src/main/java/com/ssafy/ssam/ssam_backend/api/controller/ScheduleController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/schedule")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 public class ScheduleController {
 
