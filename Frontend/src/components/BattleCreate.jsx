7128b762dc5c03e5f56718a3a054428a1ffe8062
이정환 <ready2start@naver.com>
2022-05-18 01:54:43 +0900

    [FE] :sparkles: feat: 배틀 커뮤니티 글 생성 구현

--- Frontend/src/components/BattleCreate.jsx
+++ Frontend/src/components/BattleCreate.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Box, Button, Divider, Typography } from "@mui/material";
-import { FormControl, TextField } from "@mui/material";
+import { FormControl, TextField, Select, InputLabel } from "@mui/material";
+import { MenuItem } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import { useSelector } from "react-redux";
@@ -11,17 +12,63 @@ import axios from "axios";
 
 const PostCreate = props => {
   const navigate = useNavigate();
-  const user = useSelector(state => state.user);
+  const userId = useSelector(state => state.user.userId);
   const [title, setTitle] = useState("");
+  const [myTeamId, setMyTeamId] = useState(1);
+  const [teamList, setTeamList] = useState([]);
 
-  const handleFormInput = event => {
+  // const [form, setForm] = useState({
+  //   title: "",
+  //   myTeamId: undefined,
+  // });
+
+  useEffect(() => {
+    getMyTeamList();
+  }, []);
+
+  // 글 작성자의 나만의 팀 목록을 가져온다.
+  const getMyTeamList = () => {
+    axios({
+      baseURL: process.env.REACT_APP_SERVER_URL,
+      timeout: 3000,
+      method: "GET",
+      url: `myteam/userTeamList/${userId}`,
+    })
+      .then(res => {
+        let teamList = res.data;
+        setTeamList(teamList);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const mockData = [
+    { myTeamId: 1, myTeamName: "싸피 이글스" },
+    { myTeamId: 2, myTeamName: "싸피 타이거즈" },
+    { myTeamId: 3, myTeamName: "토트넘" },
+    { myTeamId: 4, myTeamName: "리버풀" },
+  ];
+
+  const handleTitleInput = event => {
     setTitle(event.target.value);
+  };
+
+  const handleTeamSelect = event => {
+    setMyTeamId(event.target.value);
+
+    // TODO: 화면에 팀 정보를 표 형태로 보여준다.
   };
 
   // 게시글 등록
   const handleSubmitClick = () => {
     if (title === "") {
       alert("제목을 입력해주세요.");
+      return;
+    }
+
+    if (myTeamId === "") {
+      alert("나의 팀을 선택해주세요.");
       return;
     }
 
@@ -32,15 +79,12 @@ const PostCreate = props => {
       url: "/battle/post",
       data: {
         bbTitle: title,
-        // 나의 팀 ID 선택하는 부분 추가하기!
-        // myTeamId, userId
-        myTeamId: 1,
-        userId: 1,
+        myTeamId: myTeamId,
+        userId: userId,
       },
     })
       .then(res => {
         navigate("/board/battle/");
-        // 목록으로 이동
         console.log(res.data.message);
       })
       .catch(err => {
@@ -103,6 +147,9 @@ const PostCreate = props => {
           {/* TODO: 제목/내용 입력시 outline 표시하기 */}
           {/* 제목 입력창 */}
           <FormControl sx={{ mb: 2 }} fullWidth>
+            <Typography sx={{ mb: 1 }} variant="h6">
+              Title
+            </Typography>
             <TextField
               sx={{
                 borderRadius: 4,
@@ -114,7 +161,7 @@ const PostCreate = props => {
               id="title"
               required
               value={title}
-              onChange={handleFormInput}
+              onChange={handleTitleInput}
               placeholder="제목을 입력해 주세요."
               variant="standard"
               InputProps={{
@@ -122,28 +169,24 @@ const PostCreate = props => {
               }}
             />
           </FormControl>
-          {/* 내용 입력창 */}
-          {/* <FormControl fullWidth>
-            <TextField
-              sx={{
-                borderRadius: 4,
-                backgroundColor: "white",
-                disableUnderline: true,
-                p: 2,
-              }}
-              id="content"
-              required
-              value={form.content}
-              onChange={handleFormInput}
-              placeholder="내용을 입력해 주세요."
-              multiline
-              rows={10}
-              variant="standard"
-              InputProps={{
-                disableUnderline: true,
-              }}
-            />
-          </FormControl> */}
+          {/* 나만의 팀 선택 */}
+          <FormControl fullWidth>
+            {/* <InputLabel>나의 팀을 선택해 주세요.</InputLabel> */}
+            <Typography sx={{ mb: 1 }} variant="h6">
+              ⚾ My Team ⚾
+            </Typography>
+            <Select id="myTeam" value={myTeamId} onChange={handleTeamSelect}>
+              {/* <MenuItem disabled value={-1}>
+                <em>나의 팀을 선택해 주세요.</em>
+              </MenuItem> */}
+              {mockData.map(data => (
+                <MenuItem value={data.myTeamId} key={data.myTeamId}>
+                  {data.myTeamName}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {/* <Select>asdf</Select> */}
         </Box>
       </Box>
     </Box>
