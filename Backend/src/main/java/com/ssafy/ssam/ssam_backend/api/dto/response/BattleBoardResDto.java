fb255b8c96544d920bda6cd5c7f546240f164548
여율 <dbfm2222@gmail.com>
2022-05-19 19:52:32 +0900

    [BE] :bug: fix: 배틀보드 정보 dto 수정

--- Backend/src/main/java/com/ssafy/ssam/ssam_backend/api/dto/response/BattleBoardResDto.java
+++ Backend/src/main/java/com/ssafy/ssam/ssam_backend/api/dto/response/BattleBoardResDto.java
@@ -2,6 +2,7 @@ package com.ssafy.ssam.ssam_backend.api.dto.response;
 
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.ssafy.ssam.ssam_backend.domain.RoleType;
 import com.ssafy.ssam.ssam_backend.domain.entity.BattleBoard;
@@ -22,7 +23,22 @@ public class BattleBoardResDto extends BaseResponseBody {
 	private LocalDateTime bbWriteTime;
 	private LocalDateTime bbUpdateTime;
 	private RoleType role;
-	private MyTeam myTeam;
+	private String teamName;
+	private List<MyTeamPlayerResDto> myTeamPlayerList;
+	
+	public BattleBoardResDto(Integer statusCode, String message, BattleBoard board, List<MyTeamPlayerResDto> myTeamPlayerList, String teamName) {
+		this.message = message;
+		this.statusCode = statusCode;
+		this.battleBoardId = board.getBattleBoardId();
+		this.bbTitle = board.getBbTitle();
+		this.nickname = board.getAuthor().getNickname();
+		this.username = board.getAuthor().getUsername();
+		this.bbUpdateTime = board.getBbUpdateTime();
+		this.bbWriteTime = board.getBbWriteTime();
+		this.role = board.getAuthor().getRole();
+		this.teamName = teamName;
+		this.myTeamPlayerList = myTeamPlayerList;
+	}
 	
 	public BattleBoardResDto(Integer statusCode, String message, BattleBoard board) {
 		this.message = message;
@@ -34,7 +50,5 @@ public class BattleBoardResDto extends BaseResponseBody {
 		this.bbUpdateTime = board.getBbUpdateTime();
 		this.bbWriteTime = board.getBbWriteTime();
 		this.role = board.getAuthor().getRole();
-		this.myTeam = board.getMyTeam();
 	}
-	
 }
